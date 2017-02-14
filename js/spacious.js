ea46d645d5b9c78ee8d71bd54a8dbb49c97e2b5d
supernlogn <ioan.athanasiadis@gmail.com>
2017-02-14 19:12:44 +0200

    modular js

---
+++ js/spacious.js
@@ -0,0 +1,106 @@
+
+
+/**
+ * 
+ * 
+ * @param {String} jsonString a string of a json object which is
+ *                  an array of records.
+ * @param {HtmlElement} container an html container where this table will
+ *                      be contained.
+ */
+function jsonToHtmlTable(jsonString, container) {
+
+    let jsonObject = JSON.stringify(jsonString); 
+    let objectRows = jsonObject.tableData; 
+    let el = objectRows[0];
+    let htmlTableWrapper = document.createElement('div');
+        htmlTableWrapper.class = "table-responsive";
+        
+    let htmlTable = document.createElement('table');
+        htmlTable.class = "table table-bordered table-hover table-striped";
+        htmlTableWrapper.appendChild(htmlTable);
+
+    let titleThread = document.createElement('thread');
+        titleThread.appendChild(document.createElement('tr'));
+        htmlTable.appendChild(titleThread);
+    let th;
+
+    for(let property in el) {
+        th = document.createElement('th');
+        th.innerHTML = '' + property;
+        titleThread.firstChild.appendChild(th);
+    }
+    
+    let body = document.createElement('tbody');
+    htmlTable.appendChild(body);
+    
+    for(let el of objectRows) {
+        let tr = document.createElement('tr');
+        body.appendChild(tr);
+        let trInnerHTML = [];
+        for(let val of el) {
+            trInnerHTML.push('<td>'+val+'</td');
+        }
+        tr.innerHTML = trInnerHTML.join('');
+    }
+
+    let divWrapper  = document.createElement('div');
+        divWrapper.class="col-lg-4";
+        divWrapper.appendChild(htmlTableWrapper);
+        divWrapper.name = 
+    container.appendChild(divWrapper);
+}
+
+function addEventsToRow(bootstrapTable, allowDelete, allowEdit) {
+    // let tds = htmlRow.getElementsByTagName('td');
+    // for( td in tds) {
+    //     tds[td] = tds[td].innerHTML;
+    // }
+    if(allowDelete) {
+        // add an event to POST deletion data at change
+        changeHandler = function(rowData, $element) {
+            // rowData: the record corresponding to the clicked row, 
+            // $element: the tr element.
+            $.post( {
+                url: "serverApplication.php",  //server script to process data
+                data: {rowData:rowData},
+                callback: function() {
+                    console.log("sucessfull post of table data");
+                }.bind($elemeDatant),
+                cache: false
+            }).done(function() {
+                $.get({
+                    url: "serverApplication.php",  //server script to process data
+                    {tablename:, rowData:rowData},
+                    cache: false
+                })
+                
+                let tds = $element.getElementsByTagName('td');
+                for(i in tds) {
+                    tds[i].innerHTML = "1";
+                }
+            }.bind(htmlRow));
+        };
+        // and then get data, to update the field at the viewer's side.
+    }
+
+    if(allowEdit) {
+        // add an event to POST data at change
+        // and then get data, to update the field at the viewer's side.
+    }
+
+    bootstrapTable.bootstrapTable({
+        onClickRow: changeHandler
+    });
+    
+}
+
+function appendTable(tableName, container) {
+    $.get(
+        "serverApplication.php",
+        {tableName : tableName},
+        function(data) {
+            jsonToHtmlTable(data, container);
+        }.bind(this)
+    ).done();
+}
