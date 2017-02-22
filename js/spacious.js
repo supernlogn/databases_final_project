6126419ee94b4260e8fb95efcd04a88fb69768dd
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2017-02-22 04:25:38 +0200

    Format correctly

--- js/spacious.js
+++ js/spacious.js
@@ -1,7 +1,6 @@
 /*
     js sents data to php
     js receives from php
-
 */
 /*
 Example data sent by post request
@@ -35,58 +34,56 @@ Example data received by get request
 }
 */
 $changeHandler = function($rowElement) {
-    console.log("Executing changeHandler");   
-    // $rowElement: the tr element.
- 
+  console.log("Executing changeHandler");   
+  // $rowElement: the tr element.
 
-    var sendDataToServer = function ($element) {
-        var rowData = [];
-        for(let td of $element.children) {
-            rowData.push(td.innerHTML);
-        }
-        rowData.pop(); // remove button
-        rowData.shift(); // remove rowdata #
-        //TODO: Example
-        $.post( {
-            url: "php/serverApplication.php",  //server script to process data
-            data: { tableName: $element.name,
-                    rowData: rowData,
-                    deletion: false
-                  },
-            success: function($element) {
-                console.log("sucessfull post of table data");
-                $.get({ 
-                    url: "php/serverApplication.php",  //server script to process data
-                    data: { tableName: $element.name,
-                            rowData: rowData},
-                    success: function(stringData) {
-                        let data = JSON.parse(stringData);
-                        let tds = this.getElementsByTagName('td');
-                        console.log(data) // for debug reasons
-                        let k;
-                        for(k = 0; k < length(data.tableData); ++k ) {
-                            if(k == $element.firstChild.innerHTML) {
-                                break;
-                            }
-                        }
-                        let rowData = data.tableData[k];
-                        let j = 0;
-                        for(i in tds) {
-                            tds[i].innerHTML = rowData[j];
-                            j++;
-                        }
-                    }.bind(null, $element),
-                    cache: false
-                });            
-            }.bind(null, $element),
-            cache: false
-        }).done();
-    }.bind(null, $rowElement);
+  var sendDataToServer = function ($element) {
+    var rowData = [];
+    for(let td of $element.children) {
+      rowData.push(td.innerHTML);
+    }
+    rowData.pop(); // remove button
+    rowData.shift(); // remove rowdata #
+    //TODO: Example
+    $.post( {
+      url: "php/serverApplication.php",  //server script to process data
+      data: { tableName: $element.name,
+        rowData: rowData,
+        deletion: false
+      },
+      success: function($element) {
+        console.log("sucessfull post of table data");
+        $.get({ 
+          url: "php/serverApplication.php",  //server script to process data
+          data: { tableName: $element.name, rowData: rowData },
+          success: function(stringData) {
+            let data = JSON.parse(stringData);
+            let tds = this.getElementsByTagName('td');
+            console.log(data) // for debug reasons
+              let k;
+            for(k = 0; k < length(data.tableData); ++k ) {
+              if(k == $element.firstChild.innerHTML) {
+                break;
+              }
+            }
+            let rowData = data.tableData[k];
+            let j = 0;
+            for(i in tds) {
+              tds[i].innerHTML = rowData[j];
+              j++;
+            }
+          }.bind(null, $element),
+          cache: false
+        });            
+      }.bind(null, $element),
+      cache: false
+    }).done();
+  }.bind(null, $rowElement);
 
   // dont fire the previous wait if you were waiting
-    clearTimeout($.data(this, 'timer'));
-    var wait = setTimeout(sendDataToServer, 500); // delay after user types
-    $(this).data('timer', wait);    
+  clearTimeout($.data(this, 'timer'));
+  var wait = setTimeout(sendDataToServer, 500); // delay after user types
+  $(this).data('timer', wait);    
 }
 /*
 Example data sent by delete request
@@ -98,42 +95,41 @@ Example data sent by delete request
 */
 
 $deleteRowHandler = function($rowElement) {
-    console.log("deleteHandler");
-    //     var rowData = [];
-    //     for(let td of $rowElement.children) {
-    //         rowData.push(td.innerHTML);
-    //     }    
-    // console.log({ tableName: $rowElement.name,
-    //                 rowData: rowData,
-    //                 deletion: true
-    //             });
-    var sendDataRemovalToServer = function ($element) {
-        var rowData = [];
-        for(let td of $element.children) {
-            rowData.push(td.innerHTML);
-        }
-        rowData.pop(); // remove button
-        rowData.shift(); // remove rowdata #
-        //TODO: Example
-        $.post( {
-            url: "php/serverApplication.php",  //server script to process data
-            data: { tableName: $element.name,
-                    rowData: rowData,
-                    deletion: true
-                },
-            success: function($element) {
-                console.log("sucessfull deletion of table data");
-                $rowElement.remove(); // remove html data
-            }.bind(null, $element),
-            cache: false
-        }).done();
-    }.bind(null, $rowElement);
-
+  console.log("deleteHandler");
+  //     var rowData = [];
+  //     for(let td of $rowElement.children) {
+  //         rowData.push(td.innerHTML);
+  //     }    
+  // console.log({ tableName: $rowElement.name,
+  //                 rowData: rowData,
+  //                 deletion: true
+  //             });
+  var sendDataRemovalToServer = function ($element) {
+    var rowData = [];
+    for(let td of $element.children) {
+      rowData.push(td.innerHTML);
+    }
+    rowData.pop(); // remove button
+    rowData.shift(); // remove rowdata #
+    //TODO: Example
+    $.post( {
+      url: "php/serverApplication.php",  //server script to process data
+      data: { tableName: $element.name,
+        rowData: rowData,
+        deletion: true
+      },
+      success: function($element) {
+        console.log("sucessfull deletion of table data");
+        $rowElement.remove(); // remove html data
+      }.bind(null, $element),
+      cache: false
+    }).done();
+  }.bind(null, $rowElement);
 
   // dont fire the previous wait if you were waiting
-    clearTimeout($.data(this, 'timer'));
-    var wait = setTimeout(sendDataRemovalToServer, 500); // delay after user types
-    $(this).data('timer', wait);      
+  clearTimeout($.data(this, 'timer'));
+  var wait = setTimeout(sendDataRemovalToServer, 500); // delay after user types
+  $(this).data('timer', wait);      
 }
 /**
  * 
@@ -145,103 +141,101 @@ $deleteRowHandler = function($rowElement) {
  */
 var objectRows;
 function jsonToHtmlTable(jsonString, container) {
+  let jsonObject = JSON.parse(jsonString);
+  objectRows = jsonObject.tableData;
 
-    let jsonObject = JSON.parse(jsonString);
-    objectRows = jsonObject.tableData;
+  let el = objectRows[0];
 
-    let el = objectRows[0];
+  let htmlTableWrapper = document.createElement('div');
+  htmlTableWrapper.className = "table-responsive";
 
-    let htmlTableWrapper = document.createElement('div');
-        htmlTableWrapper.className = "table-responsive";
+  let htmlTable = document.createElement('table');
+  htmlTable.id = 'table_' + jsonObject.tableName;
+  htmlTable.className = "table table-bordered table-hover table-striped";
+  htmlTableWrapper.appendChild(htmlTable);
 
-    let htmlTable = document.createElement('table');
-        htmlTable.id = 'table_' + jsonObject.tableName;
-        htmlTable.className = "table table-bordered table-hover table-striped";
-        htmlTableWrapper.appendChild(htmlTable);
+  let titleThead = document.createElement('thead');
+  let headerTr = document.createElement('tr');
+  titleThead.appendChild(headerTr);
+  htmlTable.appendChild(titleThead);
+  let th;
 
-    let titleThead = document.createElement('thead');
-    let headerTr = document.createElement('tr');
-        titleThead.appendChild(headerTr);
-        htmlTable.appendChild(titleThead);
-    let th;
-
+  th = document.createElement('th');
+  th.innerHTML = '#';
+  headerTr.appendChild(th);
+  let i = 0;
+  for(let property in el) {
     th = document.createElement('th');
-    th.innerHTML = '#';
+    th["data-field"] = property;
+    // th["data-index"] = i;
+    th.data_field = property;
+    console.log(property);
+    console.log(th);
+    console.log(th["data-field"]);
+    th.innerHTML = property;
     headerTr.appendChild(th);
-    let i = 0;
-    for(let property in el) {
-        th = document.createElement('th');
-        th["data-field"] = property;
-        // th["data-index"] = i;
-        th.data_field = property;
-        console.log(property);
-        console.log(th);
-        console.log(th["data-field"]);
-        th.innerHTML = property;
-        headerTr.appendChild(th);
+  }
+  console.log(headerTr.innerHTML);
+  console.log(objectRows);
+
+  // $(function() {
+  //     $('#' + htmlTable.id).bootstrapTable({
+  //         data: objectRows
+  //     });
+  //     console.log(objectRows);
+  // });
+  let body = document.createElement('tbody');
+  htmlTable.appendChild(body);
+
+  i = 0;
+  let tr;
+  for(let element of objectRows) {
+    tr = document.createElement('tr');
+    tr.name = jsonObject.tableName;
+    body.appendChild(tr);
+    console.log(element)
+      th = document.createElement('th');
+    th.scope = "row";
+    th.innerHTML = i++;
+    tr.appendChild(th);
+    for(let val in element) {
+      let td = document.createElement('td');
+      td.contentEditable=true;
+      td.onkeyup = $changeHandler.bind(td, tr);
+      // td.attributes.contenteditable = true;
+      // td.onchange
+      td.draggable = true;
+      td.innerHTML = element[val];
+      tr.appendChild(td);
     }
-    console.log(headerTr.innerHTML);
-    console.log(objectRows);
+    let deleteTd = document.createElement('td');
+    tr.appendChild(deleteTd);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.type = "button";
+    deleteBtn.innerHTML = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+    deleteBtn.className = "btn btn-danger";
+    deleteBtn.onclick = $deleteRowHandler.bind(null, tr);
+    deleteTd.appendChild(deleteBtn);
+    // tr.innerHTML = trInnerHTML.join('');
+  }
 
-    // $(function() {
-    //     $('#' + htmlTable.id).bootstrapTable({
-    //         data: objectRows
-    //     });
-    //     console.log(objectRows);
-    // });
-    let body = document.createElement('tbody');
-    htmlTable.appendChild(body);
-    
-    i = 0;
-    let tr;
-    for(let element of objectRows) {
-        tr = document.createElement('tr');
-        tr.name = jsonObject.tableName;
-        body.appendChild(tr);
-        console.log(element)
-        th = document.createElement('th');
-        th.scope = "row";
-        th.innerHTML = i++;
-        tr.appendChild(th);
-        for(let val in element) {
-            let td = document.createElement('td');
-            td.contentEditable=true;
-            td.onkeyup = $changeHandler.bind(td, tr);
-            // td.attributes.contenteditable = true;
-            // td.onchange
-            td.draggable = true;
-            td.innerHTML = element[val];
-            tr.appendChild(td);
-        }
-        let deleteTd = document.createElement('td');
-            tr.appendChild(deleteTd);
-        let deleteBtn = document.createElement('button');
-        deleteBtn.type = "button";
-        deleteBtn.innerHTML = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
-        deleteBtn.className = "btn btn-danger";
-        deleteBtn.onclick = $deleteRowHandler.bind(null, tr);
-        deleteTd.appendChild(deleteBtn);
-        // tr.innerHTML = trInnerHTML.join('');
-   }
+  let divWrapper  = document.createElement('div');
+  divWrapper.class="col-lg-4";
+  divWrapper.appendChild(htmlTableWrapper);
 
-    let divWrapper  = document.createElement('div');
-        divWrapper.class="col-lg-4";
-        divWrapper.appendChild(htmlTableWrapper);
-
-        divWrapper.name = jsonObject.tableName;
-        container.appendChild(divWrapper);
+  divWrapper.name = jsonObject.tableName;
+  container.appendChild(divWrapper);
 }
 
 function appendTable(container, tableName) {
-    console.log("called");
-    $.get({
-        url: "php/serverApplication.php",
-        data: {tableName : tableName},
-        success: function(data) {
-            jsonToHtmlTable(data, container);
-        }
-    }).done();
-
+  console.log("called");
+  $.get({
+    url: "php/serverApplication.php",
+    data: {tableName : tableName},
+    success: function(data) {
+      jsonToHtmlTable(data, container);
+    }
+  }).done();
 }
 
 function fetchTableNames() {
@@ -263,4 +257,3 @@ function fetchTableNames() {
         }
     });
 }
-
